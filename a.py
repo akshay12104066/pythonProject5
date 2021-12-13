372414833ab717ba7e636dc26507f4607a23e56a
akshay12104066 <93517362+akshay12104066@users.noreply.github.com>
2021-12-13 15:35:38 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor('white')
+t.pencolor('red')
+t.speed(0)
+for i in range (150):
+    t.circle(190-1,80)
+    t.lt(98)
+    t.circle(190-1,80)
+    t.lt(18)
